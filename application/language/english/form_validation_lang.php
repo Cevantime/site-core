1725ec5c9fddd5242c94c1bb8476fad269998a37
Cevantime <thibaulttruffert@hotmail.com>
2015-11-06 00:31:26 +0100

    - ajout (incomplet) des règles de validation da la validation avancée

--- application/language/english/form_validation_lang.php
+++ application/language/english/form_validation_lang.php
@@ -1,5 +1,27 @@
 <?php
-
+$lang['required'] = "Le champs %s est obligatoire.";
+$lang['isset'] = "Vous devez renseigner le champs %s.";
+$lang['valid_email'] = "L'email entrée n'est pas valide.";
+$lang['valid_emails'] = "Le champs %s ne doit contenir que des adresses mail valides.";
+$lang['valid_url'] = "Le champs %s doit contenir une URL valide.";
+$lang['valid_ip'] = "Le champs %s doit contenir une adresse IP valide.";
+$lang['min_length'] = "Le champs %s doit contenir au moins %s caractères.";
+$lang['max_length'] = "Le champs %s ne doit pas contenir plus de %s caractères.";
+$lang['exact_length'] = "Le champs %s doit contenir exactement %s.";
+$lang['alpha'] = "Le champs %s ne doit contenir que des lettres.";
+$lang['alpha_numeric'] = "Le champs %s ne doit contenir que des caractères alpha-numériques.";
+$lang['alpha_dash'] = "Le champs %s ne doit contenir que des caractères alpha-numériques.";
+$lang['numeric'] = "Le champs %s ne doit contenir que des chiffres.";
+$lang['is_numeric'] = "Le champs %s ne doit contenir que des chiffres.";
+$lang['integer'] = "Le champs %s doit contenir un nombre entier.";
+$lang['regex_match'] = "La valeur saisie dans le champs %s n'est pas dans le bon format.";
+$lang['matches'] = "La valeur saisie dans le champs %s doit être la même que dans le champs %s.";
+$lang['is_unique'] = "Le %s que vous avez saisi existe déjà.";
+$lang['is_natural'] = "Le champs %s ne doit contenir que des nombres positifs.";
+$lang['is_natural_no_zero'] = "Le champs %s ne doit contenir que des nombres strictement positifs.";
+$lang['decimal'] = "Le champs %s ne doit contenir que des nombres décimaux.";
+$lang['less_than'] = "La valeur saisie dans le champs %s doit être inférieure à celle saisie dans le champs %s.";
+$lang['greater_than'] = "La valeur saisie dans le champs %s doit être supérieure à celle saisie dans le champs %s.";
 $lang['file_required']					= 'The field {field} is required.';
 $lang['file_size_max']					= "The file uploaded in {field} is too big (max size is {param}).";
 $lang['file_size_min']					= "The file uploaded in {field} is too small (min size is {param}).";
--- application/language/french/form_validation_lang.php
+++ application/language/french/form_validation_lang.php
@@ -23,6 +23,29 @@ $lang['is_natural_no_zero'] = "Le champs %s ne doit contenir que des nombres str
 $lang['decimal'] = "Le champs %s ne doit contenir que des nombres décimaux.";
 $lang['less_than'] = "La valeur saisie dans le champs %s doit être inférieure à celle saisie dans le champs %s.";
 $lang['greater_than'] = "La valeur saisie dans le champs %s doit être supérieure à celle saisie dans le champs %s.";
+$lang['file_required']					= 'The field {field} is required.';
+$lang['file_size_max']					= "The file uploaded in {field} is too big (max size is {param}).";
+$lang['file_size_min']					= "The file uploaded in {field} is too small (min size is {param}).";
+$lang['file_allowed_type']				= "The file uploaded in {field} should be {param}.";
+$lang['file_disallowed_type']			= "The file uploaded in {field} cannot be {param}.";
+$lang['file_image_maxdim']				= "The dimensions of the file uploaded in {field} are too big.";
+$lang['file_image_mindim']				= "The dimensions of the file uploaded in {field} are too small.";
+$lang['file_image_exactdim']			= "The file uploaded in {field} don't have the right dimensions.";
+$lang['is_exactly']						= "The field {field} contains a non valid value.";
+$lang['is_not']							= "The field {field} contains a non valid value.";
+$lang['valid_hour']						= "The field {field} must be a valid hour.";
+$lang['error_max_filesize_phpini']		= "The file exceeds the upload_max_filesize directive from php.ini.";
+$lang['error_max_filesize_form']		= "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.";
+$lang['error_partial_upload']			= "The file it's only partially uploaded.";
+$lang['error_temp_dir']					= "Temp dir error.";
+$lang['error_disk_write']				= "Disk write error.";
+$lang['error_stopped']					= "File upload stopped by extension";
+$lang['error_unexpected']				= "Unexpected file upload error. Error: {field}";
+$lang['valid_date']						= "The field {field} must be a valid date.";
+$lang['valid_range_date']				= "The field {field} must be a valid date range.";
+$lang['valid_geopos']                   = "The %s map doesn't have a correct position.";
+$lang['valid_multiple_geopos']          = "The %s map doesn't have a correct position.";
+$lang['file_maxdim']          = "The file uploaded in {field} has too big dimensions.";
 
 
 /* End of file form_validation_lang.php */
