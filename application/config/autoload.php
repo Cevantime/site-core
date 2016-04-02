0f53c1c804aa872093dfc1812123c7eb574da74e
cevantime <cevantime@cevantime-N56VZ>
2016-04-02 23:14:05 +0200

    - ajout ul et li parsing bbcode

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -55,7 +55,7 @@ $autoload['packages'] = array();
   |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
  */
 
-$autoload['libraries'] = array('session','database');
+$autoload['libraries'] = array('database');
 
 
 /*
--- dbchanges/liquibase/changeLog.sql
+++ dbchanges/liquibase/changeLog.sql
@@ -6,3 +6,11 @@ CREATE TABLE IF NOT EXISTS `configurations` (
   `description` text,
   PRIMARY KEY (`key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+  `id` varchar(40) NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `timestamp` int(10) unsigned NOT NULL DEFAULT '0',
+  `data` blob NOT NULL,
+  KEY `ci_sessions_timestamp` (`timestamp`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
